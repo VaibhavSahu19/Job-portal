76d34344f1c90b037097548c9b1fef48928fcf3d
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-12-06 10:24:07 +0530

    added routes for logout, get info, update profile, update password

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -86,4 +86,86 @@ export const login =  catchAsyncErrors(async(req, res, next) => {
         return next(new ErrorHandler("Invalid role.", 400));
     }
     sendToken(user, 200, res, "User Logged in successfully.");
-})
+});
+
+export const logout = catchAsyncErrors(async(req, res, next) => {
+    res.status(200).cookie("token", "", ).json({
+        success: true,
+        message: "User logged out successfully.",
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+});
+
+export const getUser = catchAsyncErrors(async(req, res, next) => {
+    const user = req.user;
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+
+export const updateProfile = catchAsyncErrors(async(req, res, next) => {
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        address: req.body.address,
+        coverLetter: req.body.coverLetter,
+        niches: {
+            firstNiche: req.body.firstNiche,
+            secondNiche: req.body.secondNiche,
+            thirdNiche: req.body.thirdNiche
+        }
+    }
+
+    const {firstNiche, secondNiche, thirdNiche} = newUserData.niches;
+    if(req.user.role === "Job Seeker" && (!firstNiche || !secondNiche || !thirdNiche)){
+        return next(new ErrorHandler("Please provide all your preferred job niches", 400));
+    }
+
+    if(req.files){
+        const resume = req.files.resume;
+        if(resume){
+            const currentResumeId = req.user.resume.public_id;
+            if(currentResumeId){
+                await cloudinary.uploader.destroy(currentResumeId);
+            }
+            const newResume = await cloudinary.uploader.upload(resume.tempFilePath, {
+                folder: "Job-seekers-resume",
+            });
+            newUserData.resume = {
+                public_id: newResume.public_id,
+                url: newResume.secure_url
+            }
+        }
+    }
+
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    });
+    res.status(200).json({
+        success: true,
+        user,
+        message: "Profile Updated.",
+    })
+});
+
+export const updatedPassword = catchAsyncErrors(async(req, res, next) => {
+    const user = await User.findById(req.user.id).select("+password");
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Old password is incorrect.", 400));
+    }
+
+    if(req.body.newPassword !== req.body.confirmPassword){
+        return next(new ErrorHandler("New password does not match.", 400));
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+    sendToken(user, 200, res, "Password updated successfully.");
+});
--- Backend/routes/userRouter.js
+++ Backend/routes/userRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { register, login } from "../controllers/userController.js";
+import { register, login, logout, getUser, updateProfile, updatedPassword } from "../controllers/userController.js";
+import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", isAuthenticated, logout);
+router.get("/me", isAuthenticated, getUser);
+router.put("/update/profile", isAuthenticated, updateProfile);
+router.put("/update/password", isAuthenticated, updatedPassword);
 
 
 export default router;
